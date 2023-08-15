3e36e465f4cfb0412c3d2d9aed89f78608381295
anndra17 <andra.hor@icloud.com>
2023-08-16 00:12:16 +0300

    first draft of app file

---
+++ App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Welcome to React App</h1>
+     <hr />
+     <h3>July 25, 2023</h3>
+     <hr />
+    </div>
+  );
+}
+
+export default App;
+
